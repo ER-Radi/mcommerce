aa614a5b3b2db8675f0ac94998f89208df60107b
Mohamed ER-Radi <erradi1994mohamed@gmail.com>
2020-04-23 13:27:41 +0200

    Feign - (1)listeDesProduits à partir du Microservice ClientUI (#4)

--- clientui/src/main/java/com/clientui/controller/ClientController.java
+++ clientui/src/main/java/com/clientui/controller/ClientController.java
@@ -1,14 +1,29 @@
 package com.clientui.controller;
 
+import com.clientui.beans.ProductBean;
+import com.clientui.proxies.MicroserviceProduitsProxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class ClientController {
 
+    @Autowired
+    MicroserviceProduitsProxy mProduitProxy;
+
     @RequestMapping("/")
     public String accueil(Model pModel) {
+
+        List<ProductBean> produits = mProduitProxy.listeDesProduits();
+
+        pModel.addAttribute("produits", produits);
+
         return "Accueil";
     }
+
+
 }
