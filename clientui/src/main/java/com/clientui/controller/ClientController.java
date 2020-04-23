8308fc4ee5479bf6eb74ad9af10b98e77cd7820f
Mohamed ER-Radi <erradi1994mohamed@gmail.com>
2020-04-23 18:21:05 +0200

    Feign - (2) ficheProduit (#6)

--- clientui/src/main/java/com/clientui/controller/ClientController.java
+++ clientui/src/main/java/com/clientui/controller/ClientController.java
@@ -5,6 +5,7 @@ import com.clientui.proxies.MicroserviceProduitsProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,6 +24,15 @@ public class ClientController {
         pModel.addAttribute("produits", produits);
 
         return "Accueil";
+    }
+
+    @RequestMapping("/details-produit/{id}")
+    public String ficheProduit(@PathVariable int id, Model pModel) {
+
+        ProductBean produit = mProduitProxy.recupererUnProduit(id);
+        pModel.addAttribute("produit", produit);
+
+        return "FicheProduit";
     }
 
 
