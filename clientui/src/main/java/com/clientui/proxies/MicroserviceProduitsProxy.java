a8ec3941f7b20490e66ada536fb2ecb00e7496f3
Mohamed ER-Radi <erradi1994mohamed@gmail.com>
2020-04-28 17:38:36 +0200

    Ribbon - Equilibrage de Charge entre les Instances du MS Produits (#14)

--- clientui/src/main/java/com/clientui/proxies/MicroserviceProduitsProxy.java
+++ clientui/src/main/java/com/clientui/proxies/MicroserviceProduitsProxy.java
@@ -1,13 +1,15 @@
 package com.clientui.proxies;
 
 import com.clientui.beans.ProductBean;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "microservice-produits", url = "localhost:9001", decode404 = false)
+@FeignClient(name = "microservice-produits", decode404 = false)
+@RibbonClient(name = "microservice-produits")
 public interface MicroserviceProduitsProxy {
 
     @GetMapping(value = "/Produits")
